1f230ce4bc8f5a523020e33609b65e84f3a48588
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2025-06-19 15:49:11 +0530

    19th June 2025 ATB13x Live Test

---
+++ src/JavaATB13xLiveTest/Arithmetic_and_Assignment_Operators.java
@@ -0,0 +1,17 @@
+package JavaATB13xLiveTest;
+
+public class Arithmetic_and_Assignment_Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int result_add = a + b;
+        int result_sub = a - b;
+        int result_mul = a * b;
+        int result_div = a / b;
+        int result_mod = a % b;
+
+        System.out.printf("Addition: %d Subtraction: %d Multiplication: %d Division: %d Modulus: %d", result_add, result_sub, result_mul, result_div, result_mod);
+
+
+    }
+}
