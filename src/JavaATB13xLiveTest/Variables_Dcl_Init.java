68f79708a8e92bb4360ad3c9759af1558221ce70
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2025-06-19 15:42:08 +0530

    19th June 2025 ATB13x Live Test

---
+++ src/JavaATB13xLiveTest/Variables_Dcl_Init.java
@@ -0,0 +1,23 @@
+package JavaATB13xLiveTest;
+
+public class Variables_Dcl_Init {
+    public static void main(String[] args) {
+        boolean bool = true;
+        byte b = 4;
+        char c = 'A';
+        double d = 3.14;
+        float f = 22.7f;
+        int i = 25;
+        long l = 12L;
+        short s = 7;
+
+        System.out.println("Boolean: " + bool);
+        System.out.println("byte: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("Double: " + d);
+        System.out.println("Float: " + f);
+        System.out.println("Integer: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Short: " + s);
+    }
+}
