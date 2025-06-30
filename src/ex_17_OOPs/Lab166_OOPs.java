a4bc0d5346f6a61bae104fa8501c59e60f28b3f5
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2025-07-01 02:26:22 +0530

    30th June 2025 Class

---
+++ src/ex_17_OOPs/Lab166_OOPs.java
@@ -0,0 +1,11 @@
+package ex_17_OOPs;
+
+public class Lab166_OOPs {
+    public static void main(String[] args) {
+
+        Person gutur = new Person();
+
+        Person abir = new Person();
+
+    }
+}
