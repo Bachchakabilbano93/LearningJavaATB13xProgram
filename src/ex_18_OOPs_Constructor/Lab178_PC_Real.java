7880165ee78d1e839622a1244efab4a4c912718b
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2025-07-03 00:53:52 +0530

    1st July 2025 Class

---
+++ src/ex_18_OOPs_Constructor/Lab178_PC_Real.java
@@ -0,0 +1,42 @@
+package ex_18_OOPs_Constructor;
+
+public class Lab178_PC_Real {
+    public static void main(String[] args) {
+        Person p1 = new Person("ABIR",89812852,"abc");
+        System.out.println(p1.name);
+        Person p2 = new Person("JOHN",74858907,"XYZ");
+
+//        Person p3  = new Person();
+//        p3.name  = "Nisha";
+//        System.out.println(p3.name);
+
+    }
+}
+
+class Person {
+
+    String name;
+    long phone;
+    String address;
+
+    Person(){
+
+    }
+
+    Person(String name_user){
+        this.name = name_user;
+    }
+
+    Person(String name_user,long phone_user,String address_user){
+        this.name = name_user;
+        this.address = address_user;
+        this.phone = phone_user;
+
+    }
+
+    Person(String name_user,long phone_user){
+        this.name = name_user;
+        this.phone = phone_user;
+
+    }
+}
