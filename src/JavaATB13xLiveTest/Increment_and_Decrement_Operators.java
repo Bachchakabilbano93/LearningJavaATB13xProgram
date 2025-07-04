ca648f89b98629c2bfb97ac2c969548f71436b9f
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2025-07-04 12:24:32 +0530

    19th June 2025 ATB13x Live Test

---
+++ src/JavaATB13xLiveTest/Increment_and_Decrement_Operators.java
@@ -0,0 +1,13 @@
+package JavaATB13xLiveTest;
+
+public class Increment_and_Decrement_Operators {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println("Original: " + i);
+        System.out.println("Pre-increment: " + ++i);
+        System.out.println("Post-increment: " + i++);
+        System.out.println("Pre-decrement: " + --i);
+        System.out.println("Post-decrement: " + i--);
+
+    }
+}
